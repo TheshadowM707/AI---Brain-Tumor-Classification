a0851d0d7461eb817a5918679889dd438c11789a
Mohammed Albasri <75527385+TheshadowM707@users.noreply.github.com>
2023-01-30 11:43:59 +0300

    Add files via upload

---
+++ AI_Training.py
@@ -0,0 +1,154 @@
+from pathlib import Path
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications import VGG16
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import AveragePooling2D
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from tqdm import tqdm
+import seaborn as sns
+
+from sklearn.utils import shuffle # Shuffle arrays or sparse matrices in a consistent way
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.models import Sequential, load_model
+
+from sklearn.metrics import classification_report, confusion_matrix
+import scikitplot as skplt
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+
+from imutils import paths;
+import matplotlib.pyplot as plt;
+import argparse;
+import os;
+import cv2;
+import numpy as np;
+
+# Load the images directories
+labels = ['glioma', 'meningioma', 'notumor', 'pituitary']
+
+X_train = [] #Training Dataset
+Y_train = [] #Training Labels
+
+for i in labels:
+    folderPath = os.path.join('C:/Xampp/htdocs/temp/alsalmaniya', 'Training', i)
+    for j in tqdm(os.listdir(folderPath)):
+        image = cv2.imread(os.path.join(folderPath, j))
+        image = cv2.resize(image, (150, 150))
+        X_train.append(image)
+        Y_train.append(i)
+
+for i in labels:
+    folderPath = os.path.join('C:/Xampp/htdocs/temp/alsalmaniya', 'Testing', i) # Join two or more pathname components
+    for j in tqdm(os.listdir(folderPath)):
+        image = cv2.imread(os.path.join(folderPath, j))
+        image = cv2.resize(image, (150, 150))
+        X_train.append(image)
+        Y_train.append(i)
+
+# Convert into numpy arrays
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+X_train, Y_train = shuffle(X_train, Y_train, random_state=42)
+
+grid_width = 4
+grid_height = 4
+f, ax = plt.subplots(grid_width, grid_height)
+f.set_size_inches(8, 8)
+
+img_idx = 0
+for i in range(0, grid_width):
+    for j in range(0, grid_height):
+        ax[i][j].axis('off')
+        ax[i][j].set_title('Label: '+Y_train[img_idx])
+        ax[i][j].imshow(X_train[img_idx])
+        img_idx += 1
+
+plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0.2, hspace=0.55)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.2, random_state=42)
+X_train, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=0.1, random_state=42)
+
+y_train_new = []
+y_valid_new = []
+y_test_new = []
+
+for i in Y_train:
+    y_train_new.append(labels.index(i))#Converting String Label to integer i.e
+                                       # glioma ---> 0, meningioma---> 1, notumor ---> 2, pituitary ---> 3
+Y_train = to_categorical(y_train_new) #Converts a class vector (integers) to binary class matrix
+
+for i in Y_valid:
+    y_valid_new.append(labels.index(i))
+
+Y_valid = to_categorical(y_valid_new)
+
+for i in Y_test:
+    y_test_new.append(labels.index(i))
+
+Y_test = to_categorical(y_test_new)
+
+Y_train
+
+model = Sequential()
+model.add(Conv2D(16, kernel_size=(3,3), activation='relu', input_shape=(150, 150, 3)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dense(4, activation='softmax'))
+
+model.compile(optimizer='adam', loss='CategoricalCrossentropy', metrics=['Acc'])
+model.summary()
+
+X_train = X_train.astype('float32')
+X_valid = X_valid.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_valid /= 255
+X_test /= 255
+
+history = model.fit(x=X_train, y=Y_train,
+                   validation_data=(X_valid, Y_valid),
+                   batch_size=32,
+                   epochs=20,
+                   verbose=1)
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+t = f.suptitle('Performance', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+epoch_list = list(range(1,21))
+ax1.plot(epoch_list, history.history['Acc'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_Acc'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(0, 21, 5))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(0, 21, 5))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
+
+model.save('AI_Model.h5')
